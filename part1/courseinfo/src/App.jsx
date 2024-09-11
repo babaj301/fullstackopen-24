b914face3db9a066bc1c87ccdb91b4acae51f3fa
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-09-11 18:42:11 +0100

    1.1 course information step 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,19 +7,39 @@ const App = () => {
   const part3 = 'State of a component';
   const exercises3 = 14;
 
+  const Header = ({ course }) => {
+    return (
+      <>
+        <h1>{course}</h1>
+      </>
+    );
+  };
+
+  const Content = ({ part, exercises }) => {
+    return (
+      <div>
+        <p>
+          {part} {exercises}
+        </p>
+      </div>
+    );
+  };
+
+  const Total = ({ ex1, ex2, ex3 }) => {
+    return (
+      <div>
+        <p>Number of exercises {ex1 + ex2 + ex3}</p>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   );
 };
