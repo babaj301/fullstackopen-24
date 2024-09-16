3f1f1b8ffd37329a31ae27ba7478b9fb068ef595
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-09-17 00:42:47 +0100

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     const exist = persons.find((person) => {
       return JSON.stringify(person.name) === JSON.stringify(newPerson.name);
@@ -22,9 +24,13 @@ const App = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleName = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -32,7 +38,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -41,7 +50,11 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => {
-          return <p key={person.name}>{person.name}</p>;
+          return (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          );
         })}
       </div>
     </div>
