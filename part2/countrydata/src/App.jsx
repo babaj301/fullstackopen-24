3ec173ede98aa7aee4a414cd1e0c7d044d8499f9
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-09-28 23:27:14 +0100

    2.18* Data for countries, step 1

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [countryValue, setCountryValue] = useState('');
+  const [allCountries, setAllCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+
+  const handleChange = (event) => {
+    setCountryValue(event.target.value);
+    const found = allCountries.filter((n) =>
+      n.name.common.toLowerCase().includes(countryValue)
+    );
+    setFiltered(found);
+
+    console.log(countryValue);
+
+    console.log(found);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((res) => {
+        setAllCountries(res.data);
+      });
+  }, []);
+
+  // console.log(allCountries);
+  // console.log(filtered);
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {' '}
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={countryValue} onChange={handleChange} />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {filtered.length === 1 ? (
+        filtered.map((n) => {
+          return (
+            <div key={n.length}>
+              <div>
+                <h1>{n.name.common}</h1>
+                <p>Capital: {n.capital.map((n) => n)}</p>
+                <p>Area: {n.area}</p>
+              </div>
 
-export default App
+              <div>
+                <h2>
+                  Languages:
+                  {/* {n.languages} */}
+                </h2>
+              </div>
+              <div>{n.flag}</div>
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          {filtered.length >= 10 ? (
+            <div>
+              <p>Too many matches, specify another filter </p>
+            </div>
+          ) : (
+            <div>
+              {filtered.map((n) => {
+                return <p key={n.ccn3}>{n.name.common}</p>;
+              })}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
