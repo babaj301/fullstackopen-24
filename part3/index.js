f46e8f35a876eca2d4fba5f63414394186d06d45
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-02 17:11:43 +0100

    3.7: Phonebook backend step 7

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const morgan = require('morgan');
 
 app.use(express.json());
+
+app.use(morgan('tiny'));
 
 let persons = [
   {
