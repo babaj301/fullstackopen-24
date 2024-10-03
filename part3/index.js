dfca9e294b01708858b6d362dc400fada5634484
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-03 10:19:29 +0100

    Running production build of frontend

--- part3/index.js
+++ part3/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use(morgan('tiny, :data'));
 
