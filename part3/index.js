aa2d04ff31d65735f5f128a384a9590014c23020
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-25 14:50:13 +0100

    3.16: Phonebook database, step 4

--- part3/index.js
+++ part3/index.js
@@ -5,10 +5,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/persons');
 
+// JSON parser to get access to body
 app.use(express.json());
+
+// for serving the frontend as a static file
 app.use(express.static('dist'));
+
+//So we can access the backend from the frontend even though its not the same domain
 app.use(cors());
 
+// A small and compact logger
 app.use(morgan('tiny, :data'));
 
 morgan.token('data', (req, res) => {
@@ -46,18 +52,21 @@ morgan.token('data', (req, res) => {
 //   //   );
 //   // });
 
+// To get all persons
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
   });
 });
 
+// To get specific person
 app.get('/api/persons/:id', (req, res) => {
   Person.findById(req.params.id).then((person) => {
     res.json(person);
   });
 });
 
+// To delete a person in the database
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then((result) => {
@@ -68,11 +77,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
     });
 });
 
-const generateId = () => {
-  const id = Math.max(...persons.map((n) => n.id)) + 1;
-  return String(id);
-};
-
+// To add a new person to the phonebook
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
@@ -86,11 +91,24 @@ app.post('/api/persons', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
-
+// For handling unknown endpoints that are not among the routes
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' });
 };
 app.use(unknownEndpoint);
 
+// To handle errors from the routes
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+// handler of requests with result to errors
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
