ea9eb1009036a114641568c5945d5a2f24939ab4
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-12-04 13:31:46 +0100

    4.4: Helper Functions and Unit Tests, step 2

---
+++ part4/tests/list_helper.test.js
@@ -0,0 +1,51 @@
+const { test, describe } = require('node:test');
+const assert = require('node:assert');
+const totalLikes = require('../utils/list_helper').totalLikes;
+
+describe('total likes', () => {
+  const emptyList = [];
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listWithMultiple = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test('of empty list is zero', () => {
+    const result = totalLikes(emptyList);
+    assert.strictEqual(result, 0);
+  });
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test('when list has multiple blogs, equals the likes of them all', () => {
+    const result = totalLikes(listWithMultiple);
+    assert.strictEqual(result, 10);
+  });
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  let total = 0;
+  blogs.map((blog) => (total += blog.likes));
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
