0c65173c760926e18e8d65415795408c7b92860f
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-05-26 15:00:59 +0100

    4.8: Blog List Tests, step 1

---
+++ part4/tests/blog_list.test.js
@@ -0,0 +1,46 @@
+const { test, after, beforeEach } = require("node:test");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const assert = require("assert");
+const app = require("../app");
+
+const api = supertest(app);
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+  },
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test("blogs are returned as json", async () => {
+  const result = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
